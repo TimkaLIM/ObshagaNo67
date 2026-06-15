db8497ff6e61a3cb136f414c260cf39bee0e67fc
Damir835 <kasimovdamir907@gmail.com>
2026-06-15 20:42:35 +0500

    добавил паузу

---
+++ Assets/Scripts pause/PauseController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class PauseController : MonoBehaviour
+{
+    [SerializeField] private GameObject pausePanel;
+    private bool isPaused = false;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void PauseGame()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
